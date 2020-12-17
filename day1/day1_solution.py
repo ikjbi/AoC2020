20b59cc77a70d4dfad44c1b942a28a0f06f45f48
ikjbi <johan.bjoerk1@gmail.com>
2020-12-17 22:17:35 +0100

    test2

--- day1/day1_solution.py
+++ day1/day1_solution.py
@@ -16,7 +16,6 @@ print(Answer)
 for it_var1 in expenses:
     for it_var2 in expenses:
         for it_var3 in expenses:
-            
-
-
-
+            if expenses[it_var1] + expenses[it_var2]+expenses[it_var3] == 2020:
+                Answer2 = int(expenses[it_var1]*expenses[it_var2]*expenses[it_var3])
+print(Answer2)
